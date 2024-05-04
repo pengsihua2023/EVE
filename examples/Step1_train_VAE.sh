30fa3678f394338dfb22074397ba75ac3601deb2
Sihua Peng <131550223+pengsihua2023@users.noreply.github.com>
2024-05-04 17:42:03 -0400

    Update Step1_train_VAE.sh

--- examples/Step1_train_VAE.sh
+++ examples/Step1_train_VAE.sh
@@ -1,10 +1,10 @@
 export MSA_data_folder='./data/MSA' # 比对好的文件
 export MSA_list='./data/mappings/example_mapping.csv' # 预先准备好的文件
 export MSA_weights_location='./data/weights' # 计算第一步就有了结果
-export VAE_checkpoint_location='./results/VAE_parameters'
+export VAE_checkpoint_location='./results/VAE_parameters' # 第一步计算完了就有了结果
 export model_name_suffix='Jan1_PTEN_example'
-export model_parameters_location='./EVE/default_model_params.json'
-export training_logs_location='./logs/'
+export model_parameters_location='./EVE/default_model_params.json' # 预先设置的文件
+export training_logs_location='./logs/' # 第一步计算完了就有了结果
 export protein_index=0
 
 python train_VAE.py \
